4231a7733e0aca9257abe7ef7839840374db25d5
Mohammad Forouhesh <mohammadh.forouhesh@gmail.com>
2023-02-23 19:20:44 +0330

    Update decoder.py

--- simple_chatbot/modeling/decoder.py
+++ simple_chatbot/modeling/decoder.py
@@ -3,19 +3,22 @@ import torch.nn as nn
 
 
 class Decoder(nn.Module):
-    def __init__(self, output_size, hidden_size):
-        super(Decoder, self).__init__()
-        self.hidden_size = hidden_size
-        self.embedding = nn.Embedding(output_size, hidden_size)
-        self.lstm = nn.LSTM(hidden_size, hidden_size)
-        self.fc = nn.Linear(hidden_size, output_size)
-        self.softmax = nn.LogSoftmax(dim=1)
-
+    def __init__(self, output_dim, emb_dim, hid_dim, n_layers, dropout):
+        super().__init__()
+        
+        self.output_dim = output_dim
+        self.hid_dim = hid_dim
+        self.n_layers = n_layers
+        self.embedding = nn.Embedding(output_dim, emb_dim)
+        self.rnn = nn.LSTM(emb_dim, hid_dim, n_layers, dropout = dropout)
+        self.fc_out = nn.Linear(hid_dim, output_dim)
+        self.dropout = nn.Dropout(dropout)
+        
     def forward(self, input, hidden, cell):
         input = input.unsqueeze(0)
-        embedded = self.embedding(input)
-        output, (hidden, cell) = self.lstm(embedded, (hidden, cell))
-        output = self.fc(output.squeeze(0))
-        output = self.softmax(output)
-        return output, hidden, cell
+        embedded = self.dropout(self.embedding(input))
+        output, (hidden, cell) = self.rnn(embedded, (hidden, cell))
+        prediction = self.fc_out(output.squeeze(0))
+        
+        return prediction, hidden, cell
 
